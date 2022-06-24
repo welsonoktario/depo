f5549cd2ea864eda26d216423ef4b4396c147100
welsonoktario <welsonoktario@yahoo.com>
2022-06-24 13:38:05 +0700

    Checkout

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,10 +1,31 @@
-import { useAtom } from "jotai"
-import React from "react"
+import { useAtom } from 'jotai'
+import React from 'react'
+import { IonButton } from '@ionic/react'
 
-import { cartAtom } from "../atoms"
+import { authAtom, cartAtom } from '../atoms'
+import { Http } from '@capacitor-community/http'
 
 const Cart: React.FC = () => {
-  const [cart] = useAtom(cartAtom)
+  const [auth] = useAtom(authAtom)
+  const [cart, setCart] = useAtom(cartAtom)
+
+  const checkout = async () => {
+    const res = await Http.post({
+      url: process.env.REACT_APP_BASE_URL + '/transaksi',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${auth.token}`,
+      },
+      data: {
+        cart,
+        user: (auth.user as any).id,
+      },
+    })
+
+    const { data } = await res.data
+    console.log(data)
+    setCart([])
+  }
 
   return (
     <>
@@ -13,6 +34,9 @@ const Cart: React.FC = () => {
           {item.barang.nama}: <span>{item.jumlah}</span>
         </p>
       ))}
+      <IonButton expand='block' onClick={checkout}>
+        Checkout
+      </IonButton>
     </>
   )
 }
